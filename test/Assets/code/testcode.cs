900397771dc86beaece1d33e68d73ded4e820ad7
LooseEnds <55034584+EmilyYB@users.noreply.github.com>
2020-01-10 13:46:24 -0500

    Initial Unity Commit

---
+++ test/Assets/code/testcode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testcode : MonoBehaviour
+{
+    public new string name = "hewwo? owo";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(name); 
+    }
+}
